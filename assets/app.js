9ec68129b303b801c0a93a55207c892930141358
Jumagreens <Jumagreens@users.noreply.github.com>
2013-11-20 15:48:05 +0300

    Adding Measure control on the map trying to find distance between two points.

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,9 @@ $(function() {
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
+  
+  //add measure control on the map
+  L.Control.measureControl().addTo(map);
 
   // add locate button
   L.control.locate().addTo(map);
